c23aaf981aceb69fbbf9b08ed515dc618c0a0d46
chanki <bucheongosok@gmail.com>
2022-07-19 16:43:55 +0900

    검색기능 추가

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import styles from "./video_item.module.css"
+import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = ({ video: { snippet } }) => { 
-    // Deconstructing:
-    // 1. props.video -> { video }
-    // 2. props.video.snippet -> { video: { snippet } }
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
-    return (
-        <li className={styles.container}>
-            <div className={styles.video}>
-                <img  className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
-                <div>
-                    <p className={styles.title} >{snippet.title}</p>
-                    <p className={styles.channel} >{snippet.channelTitle}</p>
-                </div>
-            </div>
-        </li>
-    )
-}
-
-export default VideoItem
+export default VideoItem;
