2f5814bdf42caf826d24b65a8e1faf0fe59d013a
chanki <bucheongosok@gmail.com>
2022-07-19 15:02:49 +0900

    videoItem 컴포넌트 완성

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from "./video_item.module.css"
 
-const VideoItem = (props) => {
+const VideoItem = ({ video: { snippet } }) => { 
+    // Deconstructing:
+    // 1. props.video -> { video }
+    // 2. props.video.snippet -> { video: { snippet } }
+
     return (
-        <h1>{props.video.snippet.title}</h1>
+        <li className={styles.container}>
+            <div className={styles.video}>
+                <img  className={styles.thumbnail} src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+                <div>
+                    <p className={styles.title} >{snippet.title}</p>
+                    <p className={styles.channel} >{snippet.channelTitle}</p>
+                </div>
+            </div>
+        </li>
     )
 }
 
